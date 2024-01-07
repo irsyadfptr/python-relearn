095d26c685d995a55116260c0dea93e95e3c465c
irsyadfakhrip <31012363-irsyadfakhrip@users.noreply.replit.com>
2024-01-07 14:25:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome to the Hero Name Generator.")
+city = input("What's the name of the city you want to protect?\n")
+power = input("What's your power?\n")
+gender = input("What's your gender?\n")
+print("Your Hero name could be " + city + " " + power + "-" + gender)
